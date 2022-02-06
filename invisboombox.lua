382214a38cd60d9f26736c74a67c486348ac0532
wkz6 <99113635+wkz6@users.noreply.github.com>
2022-02-06 01:50:44 -0500

    Update and rename README.md to invisboombox.lua

---
+++ invisboombox.lua
@@ -0,0 +1,35 @@
+getgenv().r = ('(p)')     
+getgenv().y = ('T') 
+getgenv().s = (' ')
+function notify(title,text,time)
+    game.StarterGui:SetCore("SendNotification", {
+        Title = title;
+        Text = text;
+        Duration = time;
+    })
+end
+notify('roys I ' .. tostring(r), 'press ' .. y .. ' to Enable/Disable.', 8)
+
+loop = false
+game:GetService('RunService').Stepped:connect(function()
+    if loop then
+            
+        local LP = game.Players.LocalPlayer
+        repeat wait() until LP.Character
+        for i,v in pairs(game.Players.LocalPlayer.Character:GetChildren()) do
+            if v.Name == "BOOMBOXHANDLE" and v:IsA("Part") then
+                v.Weld:Destroy()
+                v.Anchored = false
+                wait(2)
+                v.Anchored = true
+            end
+        end
+    end
+end)
+plr = game.Players.LocalPlayer
+mouse = plr:GetMouse()
+mouse.KeyDown:connect(function(key)
+    if key == "p" then
+        loop = not loop
+    end
+end)
